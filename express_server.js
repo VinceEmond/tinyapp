299b0475e40ea585026d18372d09964a2df51f1a
VinceEmond <vince.emond@icloud.com>
2022-02-21 21:38:20 +0000

    Added starting code

--- express_server.js
+++ express_server.js
@@ -1,1 +1,19 @@
 // express_server.js
+
+
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello Everyone!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});

389864edb18c1501cdd108ccda7d14d291cde6f7
VinceEmond <vince.emond@icloud.com>
2022-02-23 23:35:19 +0000

    Implemented route for registration, fixed header to user object instead of username, fixed logout route to clear cookies for user_id

--- express_server.js
+++ express_server.js
@@ -14,6 +14,19 @@ const urlDatabase = {
   "r3rfwr": "http://stuff.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 const generateRandomString = function() {
   return Math.random().toString(36).slice(2, 8);
 };
@@ -23,27 +36,44 @@ app.get("/", (req, res) => {
   res.redirect(`../urls`);
 });
 
+
+const fetchUserInformation = (users, userId) => {
+  let userInfo = undefined;
+
+  if (!userId) {
+    userInfo = {};
+    return userInfo;
+  }
+
+  if (users[userId]) {
+    userInfo = users[userId];
+  } else {
+    userInfo = {};
+  }
+  return userInfo;
+};
+
 // GET:BROWSE - SHOW ALL URLS
 app.get("/urls", (req,res) => {
-  const templateVars = {
-    username: req.cookies["username"],
-    urls: urlDatabase
-  };
+  const user = fetchUserInformation(users, req.cookies.user_id);
+  const templateVars = { user, urls: urlDatabase };
 
   res.render("urls_index", templateVars);
 });
 
 // GET:READ - PAGE/FORM TO CREATE NEW SHORT URL
 app.get("/urls/new", (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"]
-  };
+  const user = fetchUserInformation(users, req.cookies.user_id);
+  const templateVars = { user, urls: urlDatabase };
+
   res.render("urls_new", templateVars);
 });
 
 // GET:READ - PAGE TO VIEW SPECIFIC SHORT/LONG URL COMBO
 app.get("/urls/:shortURL", (req,res) => {
+  const user = fetchUserInformation(users, req.cookies.user_id);
   const templateVars = {
+    user,
     username: req.cookies["username"],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]};
@@ -52,9 +82,9 @@ app.get("/urls/:shortURL", (req,res) => {
 
 // GET:READ 404 ERROR PAGE
 app.get("/404", (req,res)=> {
-  const templateVars = {
-    username: req.cookies["username"],
-  };
+  const user = fetchUserInformation(users, req.cookies.user_id);
+  const templateVars = { user, urls: urlDatabase };
+
   res.render("404", templateVars);
 });
 
@@ -70,18 +100,14 @@ app.get("/u/:shortURL", (req,res) => {
 });
 
 
-
-// GET: READ - LOGIN PAGE
-app.get("/login", (req,res) => {
-  const templateVars = {
-    username: req.cookies["username"],
-    urls: urlDatabase
-  };
-  res.render("login", templateVars);
-});
-
-
-
+// // GET: READ - LOGIN PAGE
+// app.get("/login", (req,res) => {
+//   const templateVars = {
+//     username: req.cookies["username"],
+//     urls: urlDatabase
+//   };
+//   res.render("login", templateVars);
+// });
 
 // GET: READ - REGISTRATION PAGE
 app.get("/register", (req,res) => {
@@ -92,10 +118,15 @@ app.get("/register", (req,res) => {
   res.render("register", templateVars);
 });
 
-
-
-
-
+// POST:ADD - CREATE NEW USER
+app.post("/register", (req,res) => {
+  const {email, password} = req.body;
+  const userId = generateRandomString();
+  users[userId] = {id: userId, email, password};
+  res.cookie("user_id", userId);
+  // console.log(`\n****** New User Registered! ******** \n UserId: ${userId}\n Email: ${email}\n Password: ${password}\n`);
+  res.redirect("/urls");
+});
 
 // POST:EDIT - LONG URL FOR EXISTING SHORT URL
 app.post("/urls/:shortURL", (req,res) => {
@@ -130,10 +161,9 @@ app.post("/login", (req,res) => {
 
 // POST: LOGOUT USER
 app.post("/logout", (req,res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

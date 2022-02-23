c0e66b10be3a98c2f065f827a72b9efec00d9576
VinceEmond <vince.emond@icloud.com>
2022-02-23 01:42:47 +0000

    Completed implementing login/logout for username

--- express_server.js
+++ express_server.js
@@ -1,11 +1,10 @@
-// express_server.js
-
-
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -20,31 +19,43 @@ const generateRandomString = function() {
 };
 
 
-
 app.get("/", (req, res) => {
   res.redirect(`../urls`);
 });
 
 // GET:BROWSE - SHOW ALL URLS
 app.get("/urls", (req,res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
+
   res.render("urls_index", templateVars);
 });
 
 // GET:READ - PAGE/FORM TO CREATE NEW SHORT URL
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 // GET:READ - PAGE TO VIEW SPECIFIC SHORT/LONG URL COMBO
 app.get("/urls/:shortURL", (req,res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
 // GET:READ 404 ERROR PAGE
 app.get("/404", (req,res)=> {
-  res.render("404");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("404", templateVars);
 });
 
 // GET:REDIRECT - VISIT ORIGINAL WWW LONG URL
@@ -85,14 +96,18 @@ app.post("/urls/:shortURL/delete", (req,res) => {
 
 // POST: LOGIN USER
 app.post("/login", (req,res) => {
-
-  // const username = res.cookie;
   res.cookie("username",req.body.username);
   console.log(`Set the cookie! to ${req.body.username}`);
-  // console.log("Username is", req.body.username);
-  //res.end("Made it!");
   res.redirect("/urls");
 });
+
+// POST: LOGOUT USER
+app.post("/logout", (req,res) => {
+  res.clearCookie("username");
+  console.log(`Cleared the cookie: ${req.body.username}`);
+  res.redirect("/urls");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

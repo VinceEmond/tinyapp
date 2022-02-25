ad3bba7b13b20e2a95764aa29afdb7e985eacb35
VinceEmond <vince.emond@icloud.com>
2022-02-25 02:56:06 +0000

    Refactored code to clean-up the routes and comment-out any testing console.logs

--- express_server.js
+++ express_server.js
@@ -17,13 +17,6 @@ app.use(cookieSession({
   keys: ['iLikEturt13z!'],
 }));
 
-
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com",
-//   "r3rfwr": "http://stuff.com"
-// };
-
 const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
@@ -56,23 +49,7 @@ const generateRandomString = function() {
   return Math.random().toString(36).slice(2, 8);
 };
 
-const fetchUserInformation = (users, userId) => {
-  let userInfo = undefined;
-
-  if (!userId) {
-    userInfo = {};
-    return userInfo;
-  }
-
-  if (users[userId]) {
-    userInfo = users[userId];
-  } else {
-    userInfo = {};
-  }
-  return userInfo;
-};
-
-const findByEmail = (users, email) => {
+const getUserByEmail = (users, email) => {
   let foundUser = null;
 
   Object.keys(users).forEach(function(key) {
@@ -92,7 +69,7 @@ const createNewUser = (users, userInfo) => {
   }
 
   // Catch if email already exists in users database
-  if (findByEmail(users, data.email)) {
+  if (getUserByEmail(users, data.email)) {
     return {error: `Email "${data.email}" is already registered to a user!`, data: null};
   }
   
@@ -102,21 +79,21 @@ const createNewUser = (users, userInfo) => {
   const newUser = {id, email: data.email, password: hashedPassword};
   users[id] = newUser;
 
-  // console.log(`Completed "createNewUser for email: ${data.email} with error: ${error}`);
+  // console.log(`Completed "createNewUser for email: ${data.email}`);
   return {error: null, data: newUser};
 };
 
 const completedForm = (userInfo) => {
   const {email, password} =  userInfo;
-  const formIncomplete = "The form is incomplete.";
 
-  // Catch if email or password is blank
+  // Catch if email or password field is blank
   if (!email) {
-    return {error: `${formIncomplete} Please enter your email!`, data: null};
+    return {error: `The form is incomplete: Please enter your email!`, data: null};
   } else if (!password) {
-    return {error: `${formIncomplete} Please enter your password!`, data: null};
+    return {error: `The form is incomplete: Please enter your password!`, data: null};
   }
 
+  // console.log(`All inputs for the form were filled`);
   return {error: null, data: {email, password}};
 };
 
@@ -128,24 +105,25 @@ const loginUser = (users, userInfo) => {
   }
 
   // Catch if email does not exists in users database
-  if (!findByEmail(users, data.email)) {
+  if (!getUserByEmail(users, data.email)) {
     return {error: `Email "${data.email}" does not exist in our database!`, data: null};
   }
 
   const formData = data;
-  const databaseUser = findByEmail(users, formData.email);
+  const databaseUser = getUserByEmail(users, formData.email);
 
   // Catch non-matching password
   if (!bcrypt.compareSync(formData.password, databaseUser.password)) {
     return {error: `Pasword is incorrect!`, data: null};
   }
 
-  return {error: null, data:databaseUser};
+  return {error: null, data: databaseUser};
 };
 
 const urlsForUser = (id) => {
   const userUrls = {};
 
+  // Loop through database to collect all URLs associated to the id
   for (const key in urlDatabase) {
     if (urlDatabase[key].userID === id) {
       userUrls[key] = urlDatabase[key];
@@ -162,22 +140,22 @@ app.get("/", (req, res) => {
 
 // GET:BROWSE - SHOW ALL URLS
 app.get("/urls", (req,res) => {
-  const user = fetchUserInformation(users, req.session.user_id);
-  let userUrls = {};
-
+  const user = users[req.session.user_id] ? users[req.session.user_id] : {};
+  
   // If user is logged-in fetch their personal URLs
+  let userUrls = {};
   if (user.id) {
     userUrls = urlsForUser(user.id);
   }
 
+  // Pass userUrls to templateVars
   const templateVars = { user, urls: userUrls };
-
-  res.render("urls_index", templateVars);
+  return res.render("urls_index", templateVars);
 });
 
 // GET:READ - PAGE/FORM TO CREATE NEW SHORT URL
 app.get("/urls/new", (req, res) => {
-  const user = fetchUserInformation(users, req.session.user_id);
+  const user = users[req.session.user_id] ? users[req.session.user_id] : {};
   const templateVars = { user, urls: urlDatabase };
 
   // Catch if user is not logged-in
@@ -186,16 +164,16 @@ app.get("/urls/new", (req, res) => {
     return res.redirect("/login");
   }
 
-  res.render("urls_new", templateVars);
+  return res.render("urls_new", templateVars);
 });
 
 // GET:READ - PAGE TO VIEW SPECIFIC SHORT/LONG URL COMBO
 app.get("/urls/:shortURL", (req,res) => {
-  const user = fetchUserInformation(users, req.session.user_id);
+  const user = users[req.session.user_id] ? users[req.session.user_id] : {};
 
   // Catch if shortURL does not exist
   if (!urlDatabase[req.params.shortURL]) {
-    let error = `This is an invalid tiny url!`;
+    let error = `Error: Invalid tiny url!`;
     console.log(error);
     res.statusMessage = error;
     return res.status(404).send(error);
@@ -226,45 +204,52 @@ app.get("/urls/:shortURL", (req,res) => {
     longURL: urlDatabase[req.params.shortURL].longURL
   };
 
-  res.render("urls_show", templateVars);
+  return res.render("urls_show", templateVars);
 });
 
 // GET:READ 404 ERROR PAGE
 app.get("/404", (req,res)=> {
-  const user = fetchUserInformation(users, req.session.user_id);
+  const user = users[req.session.user_id] ? users[req.session.user_id] : {};
   const templateVars = { user, urls: urlDatabase };
 
-  res.render("404", templateVars);
+  return res.render("404", templateVars);
 });
 
 // GET:REDIRECT - VISIT ORIGINAL WESBSITE OF LONG URL
 app.get("/u/:shortURL", (req,res) => {
 
-  if (!(urlDatabase[req.params.shortURL])) {
-    res.redirect(`../404`);
+  // Catch if shortURL does not exist
+  if (!urlDatabase[req.params.shortURL]) {
+    let error = `Error: Invalid tiny url!`;
+    console.log(error);
+    res.statusMessage = error;
+    return res.status(404).send(error);
+  //return res.redirect("/login");
   }
 
   const longURL = urlDatabase[req.params.shortURL].longURL;
-  res.redirect(`${longURL}`);
+  return res.redirect(`${longURL}`);
 });
 
 // GET: READ - REGISTRATION PAGE
 app.get("/register", (req,res) => {
-  const user = fetchUserInformation(users, req.session.user_id);
-  const templateVars = { user, urls: urlDatabase };
+  const user = users[req.session.user_id] ? users[req.session.user_id] : {};
 
   // Catch if user is already logged-in
   if (user.id) {
-    console.log(`User with email ${user.email} (user.id: ${user.id}) is already logged-in! Redirecting to /urls.`);
-    return res.redirect("/urls");
+    let error = `Error: You are already logged-in with the email ${user.email}!`;
+    console.log(error);
+    res.statusMessage = error;
+    return res.status(401).send(error);
   }
 
-  res.render("register", templateVars);
+  const templateVars = { user, urls: urlDatabase };
+  return res.render("register", templateVars);
 });
 
 // GET: READ - LOGIN PAGE
 app.get("/login", (req,res) => {
-  const user = fetchUserInformation(users, req.session.user_id);
+  const user = users[req.session.user_id] ? users[req.session.user_id] : {};
   const templateVars = { user, urls: urlDatabase };
 
   // Catch if user is already logged-in
@@ -273,13 +258,15 @@ app.get("/login", (req,res) => {
     return res.redirect("/urls");
   }
 
-  res.render("login", templateVars);
+  return res.render("login", templateVars);
 });
 
 // POST:EDIT - LONG URL FOR EXISTING SHORT URL
 app.post("/urls/:shortURL", (req,res) => {
-  const user = fetchUserInformation(users, req.session.user_id);
- 
+  const user = users[req.session.user_id] ? users[req.session.user_id] : {};
+  const shortURL = req.params.shortURL;
+  const newURL = req.body.newURL;
+
   // Catch if user is not logged-in
   if (!user.id) {
     let error = `You are not logged-in! Redirecting to login page.`;
@@ -298,26 +285,24 @@ app.post("/urls/:shortURL", (req,res) => {
     //return res.redirect("/login");
   }
 
-  // Catch if user does not match the creator of the URL
-  if (user.id !== urlDatabase[req.params.shortURL].userID) {
-    let error = `This tiny url does not belong to this account!`;
+  // Catch if current user does not match the creator of URL
+  if (user.id !== urlDatabase[shortURL].userID) {
+    let error = `You do not have permission to edit this tinyurl!`;
     console.log(error);
     res.statusMessage = error;
     return res.status(401).send(error);
-  //return res.redirect("/login");
+    //return res.redirect("/login");
   }
-
-  const shortURL = req.params.shortURL;
-  const newURL = req.body.newURL;
+  
   urlDatabase[shortURL].longURL = newURL;
-  console.log(`Edited change the long URL to ${newURL} for shortURL ${shortURL} in the urlDataBase`);
-  console.log("urlDatabase", urlDatabase);
-  res.redirect(`/urls/${shortURL}`);
+  // console.log(`Edited urlDatabase: Updated shortURL ${shortURL}'s long URL to ${newURL}`);
+  // console.log("urlDatabase", urlDatabase);
+  return res.redirect(`/urls/${shortURL}`);
 });
 
 // POST:ADD - CREATE NEW SHORT URL
 app.post("/urls", (req, res) => {
-  const user = fetchUserInformation(users, req.session.user_id);
+  const user = users[req.session.user_id] ? users[req.session.user_id] : {};
 
   // Catch if user is not logged-in
   if (!user.id) {
@@ -328,17 +313,16 @@ app.post("/urls", (req, res) => {
     //return res.redirect("/login");
   }
 
-  console.log(`Yes you are logged-in! Processing post request.`);
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = {longURL : req.body.longURL, userID: user.id};
-
-  console.log("Added a new long/short combo to the urlDataBase");
-  console.log("urlDatabase", urlDatabase);
-  res.redirect(302, `/urls/${shortURL}`);
+  // console.log("Added a new long/short combo to the urlDataBase");
+  // console.log("urlDatabase", urlDatabase);
+  return res.redirect(`/urls/${shortURL}`);
 });
 
 // POST:ADD - CREATE NEW USER
 app.post("/register", (req,res) => {
+  // Attempt to create new user; relay error if unsuccesful
   const {error, data} = createNewUser(users, req.body);
   if (error) {
     console.log(error);
@@ -346,35 +330,36 @@ app.post("/register", (req,res) => {
     return res.status(400).send(error);
   }
 
-  req.session.user_id = data.id;
-  console.log(`\n****** New User Registered! ******** \n UserId: ${data.id}\n Email: ${data.email}\n Password: ${data.password}\n`);
-  res.redirect("/urls");
+  req.session["user_id"] = data.id;
+  // console.log(`\n****** New User Registered! ******** \n UserId: ${data.id}\n Email: ${data.email}\n Password: ${data.password}\n`);
+  return res.redirect("/urls");
 });
 
 // POST:LOGIN - LOGIN TO EXISTING USER
 app.post("/login", (req,res) => {
+  // Attempt to login existing user; relay error if unsuccesful
   const {error, data} = loginUser(users, req.body);
-
   if (error) {
     console.log(error);
     res.statusMessage = error;
     return res.status(403).send(error);
   }
 
-  req.session.user_id = data.id;
-  console.log(`\n****** USER AUTHENTICATED AND LOGGED-IN! ******** \n UserId: ${data.id}\n Email: ${data.email}\n Password: ${data.password}\n`);
-  res.redirect("/urls");
+  req.session["user_id"] = data.id;
+  // console.log(`\n****** USER AUTHENTICATED AND LOGGED-IN! ******** \n UserId: ${data.id}\n Email: ${data.email}\n Password: ${data.password}\n`);
+  return res.redirect("/urls");
 });
 
 // POST:LOGOUT - LOGOUT USER BY DELETING COOKIES
 app.post("/logout", (req,res) => {
   req.session = null;
-  res.redirect("/urls");
+  return res.redirect("/urls");
 });
 
 // POST:DELETE - DELETE TINY/LONG COMBO FROM DATABASE
 app.post("/urls/:shortURL/delete", (req,res) => {
-  const user = fetchUserInformation(users, req.session.user_id);
+  const user = users[req.session.user_id] ? users[req.session.user_id] : {};
+  const shortURL = req.params.shortURL;
 
   // Catch if user is not logged-in
   if (!user.id) {
@@ -386,7 +371,7 @@ app.post("/urls/:shortURL/delete", (req,res) => {
   }
 
   // Catch if shortURL does not exist
-  if (!urlDatabase[req.params.shortURL]) {
+  if (!urlDatabase[shortURL]) {
     let error = `This is an invalid tiny url!`;
     console.log(error);
     res.statusMessage = error;
@@ -394,8 +379,8 @@ app.post("/urls/:shortURL/delete", (req,res) => {
   //return res.redirect("/login");
   }
 
-  // Catch if user does not match the creator of the URL
-  if (user.id !== urlDatabase[req.params.shortURL].userID) {
+  // Catch if current user does not match the creator of URL
+  if (user.id !== urlDatabase[shortURL].userID) {
     let error = `This tiny url does not belong to this account!`;
     console.log(error);
     res.statusMessage = error;
@@ -403,9 +388,8 @@ app.post("/urls/:shortURL/delete", (req,res) => {
     //return res.redirect("/login");
   }
 
-  const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
-  res.redirect(`/urls`);
+  return res.redirect(`/urls`);
 });
 
 app.listen(PORT, () => {
